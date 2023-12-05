c2d0f63f6543c34b0c4ca9aba014ac38c2b73e4f
Michael Neale <michael.neale@gmail.com>
2023-12-05 13:07:06 +1100

    adding in a simple fetch and check of OFAC sanctions list (#1)

--- src/example/issue-credential.ts
+++ src/example/issue-credential.ts
@@ -14,6 +14,76 @@ const issuer = await createOrLoadDid('issuer.json')
 // At this point we can check if the user is sanctioned or not and decide to issue the credential.
 // TOOD: implement the actual sanctions check!
 
+import fetch from 'node-fetch'
+import Papa from 'papaparse'
+import fuzzysort from 'fuzzysort'
+
+type SanctionEntry = {
+  name: string;
+  country: string;
+  // Additional fields can be added as needed
+}
+
+type QueryData = {
+  name: string;
+  minScore: number;
+  country?: string;
+}
+
+async function loadSanctionsData(): Promise<SanctionEntry[] | null> {
+  const url = 'https://www.treasury.gov/ofac/downloads/sdn.csv'
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const text = await response.text()
+    const data = Papa.parse<SanctionEntry>(text, { header: true }).data
+    return data
+  } catch (error) {
+    console.error(`Error fetching data: ${error}`)
+    return null
+  }
+}
+
+console.log('Loading OFAC sanctions data...')
+let sanctionsData: SanctionEntry[] | null = await loadSanctionsData()
+
+
+function searchSanctions(queryData: QueryData, data: SanctionEntry[]): SanctionEntry[] {
+  const nameResults = fuzzysort.go(queryData.name, data, { key: 'name' })
+  let results = nameResults.map(result => result.obj)
+
+  if (queryData.country) {
+    const countryResults = fuzzysort.go(queryData.country, data, { key: 'country' })
+    const countryMatched = countryResults.map(result => result.obj)
+    results = results.filter(entry => countryMatched.includes(entry))
+  }
+
+  // Apply minimum score filter
+  results = results.filter(result => {
+    const nameScore = fuzzysort.single(queryData.name, result.name)?.score ?? -Infinity
+    const countryScore = queryData.country ? (fuzzysort.single(queryData.country, result.country)?.score ?? -Infinity) : 0
+    return nameScore >= queryData.minScore || countryScore >= queryData.minScore
+  })
+
+  return results
+}
+
+// Example usage
+const sanctionsSearch = searchSanctions({
+  name: 'Alice Acme',
+  minScore: 80,
+  country: 'Australia'
+}, sanctionsData)
+
+if (sanctionsSearch.length > 0) {
+  console.log('Sanctions found for Alice Acme')
+  console.log(sanctionsSearch)
+  process.exit(-1)
+}
+
+
 
 //
 //
