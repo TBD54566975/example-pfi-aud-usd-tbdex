8c58e5cc8a94d0271f2ae52803bd469ef42383ab
Michael Neale <michael.neale@gmail.com>
2024-03-12 17:49:37 +1100

    load PFI did from a json file (#6)

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
-import type { PoolConfig } from "pg";
+import type { PoolConfig } from 'pg'
 
-import type { LogLevelDesc } from "loglevel";
-import fs from "node:fs";
+import type { LogLevelDesc } from 'loglevel'
+import fs from 'fs/promises'
 
-import "dotenv/config";
+import 'dotenv/config'
 
-import { BearerDid, DidDht } from "@web5/dids";
-import { createOrLoadDid } from "./credential-issuer.js";
+import { BearerDid, DidDht, PortableDid } from '@web5/dids'
 
-export type Environment = "local" | "staging" | "production";
+
+export type Environment = 'local' | 'staging' | 'production'
 
 export type Config = {
   env: Environment;
@@ -19,37 +19,54 @@ export type Config = {
   pfiDid: BearerDid;
   allowlist: string[];
   pinPaymentsKey: string;
-};
+}
 
 export const config: Config = {
-  env: (process.env["ENV"] as Environment) || "local",
-  logLevel: (process.env["LOG_LEVEL"] as LogLevelDesc) || "info",
-  host: process.env["HOST"] || "http://localhost:9000",
-  port: parseInt(process.env["PORT"] || "9000"),
+  env: (process.env['ENV'] as Environment) || 'local',
+  logLevel: (process.env['LOG_LEVEL'] as LogLevelDesc) || 'info',
+  host: process.env['HOST'] || 'http://localhost:9000',
+  port: parseInt(process.env['PORT'] || '9000'),
   db: {
-    host: process.env["SEC_DB_HOST"] || "localhost",
-    port: parseInt(process.env["SEC_DB_PORT"] || "5432"),
-    user: process.env["SEC_DB_USER"] || "postgres",
-    password: process.env["SEC_DB_PASSWORD"] || "tbd",
-    database: process.env["SEC_DB_NAME"] || "mockpfi",
+    host: process.env['SEC_DB_HOST'] || 'localhost',
+    port: parseInt(process.env['SEC_DB_PORT'] || '5432'),
+    user: process.env['SEC_DB_USER'] || 'postgres',
+    password: process.env['SEC_DB_PASSWORD'] || 'tbd',
+    database: process.env['SEC_DB_NAME'] || 'mockpfi',
   },
-  pfiDid: await createOrLoadDid("pfi.json"),
-  pinPaymentsKey: process.env["SEC_PIN_PAYMENTS_SECRET_KEY"],
-  allowlist: JSON.parse(process.env["SEC_ALLOWLISTED_DIDS"] || "[]"),
-};
+  pfiDid: await createOrLoadDid('pfi.json'),
+  pinPaymentsKey: process.env['SEC_PIN_PAYMENTS_SECRET_KEY'],
+  allowlist: JSON.parse(process.env['SEC_ALLOWLISTED_DIDS'] || '[]'),
+}
 
-// create ephemeral PFI did if one wasn't provided. Note: this DID and associated keys aren't persisted!
-// a new one will be generated every time the process starts.
-if (!config.pfiDid) {
-  console.log(
-    "PFI DID was not loaded from config. Creating an ephemeral PFI DID.....",
-  );
-  const pfiDid = await DidDht.create({
-    options: {
-      services: [{ id: "pfi", type: "PFI", serviceEndpoint: config.host }],
-    },
-  });
 
-  config.pfiDid = pfiDid;
-  fs.writeFileSync("pfi.json", JSON.stringify(config.pfiDid, null, 2));
+
+
+async function createOrLoadDid(filename: string): Promise<BearerDid> {
+  // Check if the file exists
+  try {
+    const data = await fs.readFile(filename, 'utf-8')
+    const portableDid: PortableDid = JSON.parse(data)
+    const bearerDid = await DidDht.import({ portableDid })
+    return bearerDid
+  } catch (error) {
+    // If the file doesn't exist, generate a new DID
+    if (error.code === 'ENOENT') {
+      console.log('Creating new did for server...')
+      const bearerDid = await DidDht.create({
+        options: {
+          services: [
+            {
+              id: 'pfi',
+              type: 'PFI',
+              serviceEndpoint: process.env['HOST'] || 'http://localhost:9000',
+            },
+          ],
+        },
+      })
+      const portableDid = await bearerDid.export()
+      await fs.writeFile(filename, JSON.stringify(portableDid, null, 2))
+      return bearerDid
+    }
+    console.error('Error reading from file:', error)
+  }
 }
--- src/credential-issuer.ts
+++ src/credential-issuer.ts
@@ -1,105 +1,96 @@
-import { BearerDid, DidDht, PortableDid } from "@web5/dids";
-import { VerifiableCredential } from "@web5/credentials";
+import { BearerDid, DidDht, PortableDid } from '@web5/dids'
+import { VerifiableCredential } from '@web5/credentials'
 
-import fetch from "node-fetch";
-import Papa from "papaparse";
-import fuzzysort from "fuzzysort";
+import fetch from 'node-fetch'
+import Papa from 'papaparse'
+import fuzzysort from 'fuzzysort'
 
-import fs from "fs/promises";
+import fs from 'fs/promises'
 
-export async function createOrLoadDid(filename: string): Promise<BearerDid> {
+async function createOrLoadDid(filename: string): Promise<BearerDid> {
   // Check if the file exists
   try {
-    const data = await fs.readFile(filename, "utf-8");
-    const portableDid: PortableDid = JSON.parse(data);
-    const bearerDid = await DidDht.import({ portableDid });
-    return bearerDid;
+    const data = await fs.readFile(filename, 'utf-8')
+    const portableDid: PortableDid = JSON.parse(data)
+    const bearerDid = await DidDht.import({ portableDid })
+    return bearerDid
   } catch (error) {
     // If the file doesn't exist, generate a new DID
-    if (error.code === "ENOENT") {
+    if (error.code === 'ENOENT') {
       const bearerDid = await DidDht.create({
-        options: {
-          services: [
-            {
-              id: "pfi",
-              type: "PFI",
-              serviceEndpoint: "http://localhost:9000",
-            },
-          ],
-        },
-      });
-      const portableDid = await bearerDid.export();
-      await fs.writeFile(filename, JSON.stringify(portableDid, null, 2));
-      return bearerDid;
+      })
+      const portableDid = await bearerDid.export()
+      await fs.writeFile(filename, JSON.stringify(portableDid, null, 2))
+      return bearerDid
     }
-    console.error("Error reading from file:", error);
+    console.error('Error reading from file:', error)
   }
 }
 
 // load issuer's did from a file called issuer-did.txt
-const issuer: BearerDid = await createOrLoadDid("issuer.json");
+const issuer: BearerDid = await createOrLoadDid('issuer.json')
 // write issuer did to file so server can trust it:
-await fs.writeFile("issuer-did.txt", issuer.uri);
+await fs.writeFile('issuer-did.txt', issuer.uri)
 
 type SanctionEntry = {
   name: string;
   country: string;
   // Additional fields can be added as needed
-};
+}
 
 type QueryData = {
   name: string;
   minScore: number;
   country?: string;
-};
+}
 
 async function loadSanctionsData(): Promise<SanctionEntry[] | null> {
-  const url = "https://www.treasury.gov/ofac/downloads/sdn.csv";
+  const url = 'https://www.treasury.gov/ofac/downloads/sdn.csv'
   try {
-    const response = await fetch(url);
+    const response = await fetch(url)
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`)
     }
-    const text = await response.text();
-    const data = Papa.parse<SanctionEntry>(text, { header: true }).data;
-    return data;
+    const text = await response.text()
+    const data = Papa.parse<SanctionEntry>(text, { header: true }).data
+    return data
   } catch (error) {
-    console.error(`Error fetching data: ${error}`);
-    return null;
+    console.error(`Error fetching data: ${error}`)
+    return null
   }
 }
 
-console.log("Loading OFAC sanctions data...");
-let sanctionsData: SanctionEntry[] | null = await loadSanctionsData();
+console.log('Loading OFAC sanctions data...')
+let sanctionsData: SanctionEntry[] | null = await loadSanctionsData()
 
 function searchSanctions(
   queryData: QueryData,
   data: SanctionEntry[],
 ): SanctionEntry[] {
-  const nameResults = fuzzysort.go(queryData.name, data, { key: "name" });
-  let results = nameResults.map((result) => result.obj);
+  const nameResults = fuzzysort.go(queryData.name, data, { key: 'name' })
+  let results = nameResults.map((result) => result.obj)
 
   if (queryData.country) {
     const countryResults = fuzzysort.go(queryData.country, data, {
-      key: "country",
-    });
-    const countryMatched = countryResults.map((result) => result.obj);
-    results = results.filter((entry) => countryMatched.includes(entry));
+      key: 'country',
+    })
+    const countryMatched = countryResults.map((result) => result.obj)
+    results = results.filter((entry) => countryMatched.includes(entry))
   }
 
   // Apply minimum score filter
   results = results.filter((result) => {
     const nameScore =
-      fuzzysort.single(queryData.name, result.name)?.score ?? -Infinity;
+      fuzzysort.single(queryData.name, result.name)?.score ?? -Infinity
     const countryScore = queryData.country
       ? fuzzysort.single(queryData.country, result.country)?.score ?? -Infinity
-      : 0;
+      : 0
     return (
       nameScore >= queryData.minScore || countryScore >= queryData.minScore
-    );
-  });
+    )
+  })
 
-  return results;
+  return results
 }
 
 /*
@@ -117,24 +108,24 @@ export async function requestCredential(
       country: country,
     },
     sanctionsData,
-  );
+  )
 
   if (sanctionsSearch.length > 0) {
-    console.log("we have a naughty person, we cannot do business with them");
-    return false;
+    console.log('we have a naughty person, we cannot do business with them')
+    return false
   }
 
   // Create a sanctions credential so that the PFI knows that Alice is legit.
   const vc = await VerifiableCredential.create({
-    type: "SanctionCredential",
+    type: 'SanctionCredential',
     issuer: issuer.uri,
     subject: customerDid,
     data: {
-      beep: "boop",
+      beep: 'boop',
     },
-  });
+  })
 
-  const vcJwt = await vc.sign({ did: issuer });
+  const vcJwt = await vc.sign({ did: issuer })
 
-  return vcJwt;
+  return vcJwt
 }
