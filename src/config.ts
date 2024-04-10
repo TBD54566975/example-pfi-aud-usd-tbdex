590589293c63ba14f180f3ba7392b4a747a2d7f0
Mic Neale <michael.neale@gmail.com>
2024-04-10 14:40:32 +1000

    remove DB config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
-import type { PoolConfig } from 'pg'
-
 import type { LogLevelDesc } from 'loglevel'
 import fs from 'fs/promises'
 
@@ -15,7 +13,6 @@ export type Config = {
   logLevel: LogLevelDesc;
   host: string;
   port: number;
-  db: PoolConfig;
   pfiDid: BearerDid;
   allowlist: string[];
   pinPaymentsKey: string;
@@ -26,13 +23,6 @@ export const config: Config = {
   logLevel: (process.env['LOG_LEVEL'] as LogLevelDesc) || 'info',
   host: process.env['HOST'] || 'http://localhost:9000',
   port: parseInt(process.env['PORT'] || '9000'),
-  db: {
-    host: process.env['SEC_DB_HOST'] || 'localhost',
-    port: parseInt(process.env['SEC_DB_PORT'] || '5432'),
-    user: process.env['SEC_DB_USER'] || 'postgres',
-    password: process.env['SEC_DB_PASSWORD'] || 'tbd',
-    database: process.env['SEC_DB_NAME'] || 'mockpfi',
-  },
   pfiDid: await createOrLoadDid('pfi.json'),
   pinPaymentsKey: process.env['SEC_PIN_PAYMENTS_SECRET_KEY'],
   allowlist: JSON.parse(process.env['SEC_ALLOWLISTED_DIDS'] || '[]'),
