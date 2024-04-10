5a0382935c522994741477cebaa9ec2e281fe08c
Mic Neale <michael.neale@gmail.com>
2024-04-10 14:55:48 +1000

    add end to end test

---
+++ src/test_end2end.cjs
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer')
+const path = require('path');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false }) // Launch the browser in non-headless mode
+  const page = await browser.newPage()
+
+  const filePath = path.join(__dirname, './didpay-client/index.html')
+  const fileUrl = `file://${filePath}`
+
+  await page.goto(fileUrl, { waitUntil: 'networkidle0' })
+
+  // Wait for offerings to load
+  await page.waitForSelector('#offeringsContainer > div')
+
+  // Check if offerings loaded at the top
+  const offeringsLoaded = await page.evaluate(() => {
+    const offeringsContainer = document.querySelector('#offeringsContainer')
+    return offeringsContainer.firstElementChild !== null
+  })
+
+  if (offeringsLoaded) {
+    console.log('Offerings loaded successfully')
+    process.exit(0)
+  } else {
+    console.error('Offerings failed to load')
+    process.exit(1)
+  }
+
+  await browser.close()
+})()
